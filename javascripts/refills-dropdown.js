911c1d3dea0942a727b7813e109f73a92ece71d1
Ricardo Gonçalves <ricardo@ricardok.com>
2016-03-07 19:17:28 -0300

    fix form get a quote

--- javascripts/refills-dropdown.js
+++ javascripts/refills-dropdown.js
@@ -8,7 +8,6 @@
       $menu.toggleClass("dropdown__menu--show");
       $button.toggleClass('dropdown__button--opened');
       $menu.children(".dropdown__menu-item").click(function () {
-        console.log($(this).data("value"));
         $('#budget').val($(this).data("value"));
         $button.removeClass('dropdown__button--opened');
         $menu.removeClass("dropdown__menu--show");
